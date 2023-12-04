cfe5fe922014e2a11059b3ad68b94b8ce5871b87
MishraAnkit8 <144013606+MishraAnkit8@users.noreply.github.com>
2023-12-04 19:08:56 +0530

    Update journal-paper.models.js

--- journal-paper.models.js
+++ journal-paper.models.js
@@ -1,25 +1,30 @@
-const { autoriders_read_db, autoriders_write_db } = require('../../config/db-configs');
-const dbPoolManager = require('../../config/db-pool-manager');
-const moment = require('moment');
+const journalPaperService = require('../services/journal-paper.service');
+// controller for fetching
+module.exports.renderJournalPaper = async (req, res, next) => {
+    const journalList = await journalPaperService.renderJournalPaper();
+    res.render('journal-paper', {
+        journalData : journalList
+    })
+}
 
-const autoDbR = dbPoolManager.get('autoDbR', autoriders_read_db);
-const autoDbW = dbPoolManager.get('autoDbW', autoriders_write_db);
+// controller for inserting
+module.exports.createJournalPaper = async (req, res, next) => {
+    const journalPaperData = await journalPaperService.insertJournalPapper(req.body) ;
+    
+    console.log('jornal paper is created ==>>' , journalPaperData);
+    if(journalPaperData && journalPaperData.rows[0].id) {
+        console.log('ID' ,journalPaperData.rows[0].id);
+        res.status(200).send({
+            'status' : 'done',
+            taskId : journalPaperData.rows[0].id
+        })
 
-module.exports.fetchJournalPaper = () => {
-    let sql = {
-        text : 'SELECT id, year, school, campus, policy_cadre, all_authors, total_authors, nmims_authors, journal_name, publisher FROM journal_papers',
+    }
+    else {
+        res.status(500).send({
+            'status' : 'failed',
+            'massage' : 'failed to insert new row'
+        });
+    }
 
-    };
-    return autoDbR.query(sql);
-};
-
-module.exports.createJournalPaper = (data) => {
-    const { year, school, campus, policy_cadre, all_authors, total_authors, nmims_authors, journal_name} = data ;
-    let sql = {
-        text : `INSERT INTO journal_papers ( year, school, campus, policy_cadre, all_authors, total_authors, nmims_authors, journal_name, publisher)  
-        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) ` ,
-        values : [year, school, campus, policy_cadre, all_authors, total_authors, nmims_authors, journal_name, publisher]
-    };
-    console.log('RowId ==>' , sql.values);
-    return autoDbW.query(sql);
 }
