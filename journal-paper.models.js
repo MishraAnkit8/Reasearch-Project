021b1ba00b24d35afcdda659a655a102f3580a62
MishraAnkit8 <144013606+MishraAnkit8@users.noreply.github.com>
2023-12-05 15:38:32 +0530

    Update journal-paper.models.js

--- journal-paper.models.js
+++ journal-paper.models.js
@@ -1,30 +1,35 @@
-const journalPaperService = require('../services/journal-paper.service');
-// controller for fetching
-module.exports.renderJournalPaper = async (req, res, next) => {
-    const journalList = await journalPaperService.renderJournalPaper();
-    res.render('journal-paper', {
-        journalData : journalList
-    })
+const { autoriders_read_db, autoriders_write_db } = require('../../config/db-configs');
+const dbPoolManager = require('../../config/db-pool-manager');
+const moment = require('moment');
+
+const autoDbR = dbPoolManager.get('autoDbR', autoriders_read_db);
+const autoDbW = dbPoolManager.get('autoDbW', autoriders_write_db);
+
+module.exports.fetchJournalPaper = () => {
+    let sql = {
+        text : 'SELECT id, year, school, campus, policy_cadre, all_authors, total_authors, nmims_authors, journal_name, publisher FROM journal_papers',
+
+    };
+    return autoDbR.query(sql);
+};
+
+module.exports.createJournalPaper = ({journalDetails}) => {
+    const { year, school, campus, policy_cadre, all_authors, total_authors, nmims_authors, journal_name, publisher} = journalDetails ;
+
+    let sql = {
+        text : `INSERT INTO journal_papers ( year, school, campus, policy_cadre, all_authors, total_authors, nmims_authors, journal_name, publisher)  
+        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING id ` ,
+        values : [year, school, campus, policy_cadre, all_authors, total_authors, nmims_authors, journal_name, publisher]
+    };
+    
+    return autoDbW.query(sql);
 }
 
-// controller for inserting
-module.exports.createJournalPaper = async (req, res, next) => {
-    const journalPaperData = await journalPaperService.insertJournalPapper(req.body) ;
-    
-    console.log('jornal paper is created ==>>' , journalPaperData);
-    if(journalPaperData && journalPaperData.rows[0].id) {
-        console.log('ID' ,journalPaperData.rows[0].id);
-        res.status(200).send({
-            'status' : 'done',
-            taskId : journalPaperData.rows[0].id
-        })
-
-    }
-    else {
-        res.status(500).send({
-            'status' : 'failed',
-            'massage' : 'failed to insert new row'
-        });
-    }
+module.exports.deleteJournalPaper =  async({taskId}) => {
+    let sql = {
+        text : `DELETE FROM journal_papers WHERE id = $1 `,
+        values : [taskId]
+    };
+    return autoDbR.query(sql);
 
 }
