97161cb979b30865cc1acf78fdc5a7b84e24e7a6
MishraAnkit8 <144013606+MishraAnkit8@users.noreply.github.com>
2023-12-05 15:38:00 +0530

    Update journal-paper.service.js

--- journal-paper.service.js
+++ journal-paper.service.js
@@ -7,9 +7,26 @@ module.exports.renderJournalPaper = async () => {
 
 module.exports.insertJournalPapper = async (body) => {
     const {journalDetails} =  body;
-    console.log('journalDetails>>> ', journalDetails);
+    console.log('journalDetails>>> ', body);
 
     const newJournalPaper = await journalPaperModel.createJournalPaper({journalDetails});
     return newJournalPaper ;
 
+};
+
+module.exports.deleteJournalPaper = async (taskId) => {
+    const result = await journalPaperModel.deleteJournalPaper({ taskId });
+    if(result.rowCount ===1){
+        return {
+            success : true ,
+            massage : 'row is deleted successfully'
+        };
+    }
+    else{
+        return {
+            success : fasle ,
+            massage : 'could not delete any thing'
+        }
+    }
+  
 };
