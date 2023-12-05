75579c6a4bfa5a9c284001f675f3b1c139bcebe8
MishraAnkit8 <144013606+MishraAnkit8@users.noreply.github.com>
2023-12-05 17:48:21 +0530

    Update journal-paper.models.js

--- journal-paper.models.js
+++ journal-paper.models.js
@@ -5,6 +5,7 @@ const moment = require('moment');
 const autoDbR = dbPoolManager.get('autoDbR', autoriders_read_db);
 const autoDbW = dbPoolManager.get('autoDbW', autoriders_write_db);
 
+// for fetching journal paper data 
 module.exports.fetchJournalPaper = () => {
     let sql = {
         text : 'SELECT id, year, school, campus, policy_cadre, all_authors, total_authors, nmims_authors, journal_name, publisher FROM journal_papers',
@@ -13,6 +14,7 @@ module.exports.fetchJournalPaper = () => {
     return autoDbR.query(sql);
 };
 
+// for inserting journal paper  data
 module.exports.createJournalPaper = ({journalDetails}) => {
     const { year, school, campus, policy_cadre, all_authors, total_authors, nmims_authors, journal_name, publisher} = journalDetails ;
 
@@ -25,6 +27,7 @@ module.exports.createJournalPaper = ({journalDetails}) => {
     return autoDbW.query(sql);
 }
 
+// for deleting journal paper  data 
 module.exports.deleteJournalPaper =  async({taskId}) => {
     let sql = {
         text : `DELETE FROM journal_papers WHERE id = $1 `,
@@ -33,3 +36,17 @@ module.exports.deleteJournalPaper =  async({taskId}) => {
     return autoDbR.query(sql);
 
 }
+
+// for updating 
+module.exports.updateJournalPaperData = async ({taskId , updateJournalDetails}) => {
+    const { year, school, campus, policy_cadre, all_authors, total_authors, nmims_authors, journal_name, publisher} = updateJournalDetails;
+    let sql = {
+         text : ` UPDATE journal_papers 
+                SET year = $2, school = $3, campus = $4, policy_cadre = $5, all_authors = $6,
+                    total_authors = $7, nmims_authors = $8, journal_name = $9, publisher = $10
+                WHERE id = $1 `,
+        values : [taskId, year, school, campus, policy_cadre, all_authors, total_authors, nmims_authors, journal_name, publisher]
+    };
+    return autoDbW.query(sql);
+    
+};
