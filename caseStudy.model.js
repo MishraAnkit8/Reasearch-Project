34ba7433706cb1ccaaafcd47b47145848443610f
MishraAnkit8 <144013606+MishraAnkit8@users.noreply.github.com>
2023-12-09 18:51:35 +0530

    Update caseStudy.model.js

--- caseStudy.model.js
+++ caseStudy.model.js
@@ -1,29 +1,48 @@
-const { pgPool } = require('../../config/db.config');
+const { autoriders_read_db, autoriders_write_db } = require('../../config/db-configs');
+const dbPoolManager = require('../../config/db-pool-manager');
+const moment = require('moment');
 
-module.exports.fetchAll = async () => {
-  return pgPool.query('SELECT id, fname, lname, title_of_case_study, edition, volume, publisher_name, publication_year FROM case_studies ORDER BY id;');
+const autoDbR = dbPoolManager.get('autoDbR', autoriders_read_db);
+const autoDbW = dbPoolManager.get('autoDbW', autoriders_write_db);
+
+module.exports.renderCaseStudy = () => {
+    let sql = {
+        text : `SELECT id,  author_fname, author_lname, title_of_case_study, edition, volume_number, publisher_name, publication_year FROM case_studies ORDER BY id `
+    }
+    return autoDbR.query(sql);
 };
 
-module.exports.createCaseStudy = (data) => {
-    console.log("Big DATA is>>>>>", data);
-  const {
-    fname,
-    lname,
-    title_of_case_study,
-    edition,
-    volume,
-    publisher_name,
-    publication_year,
-  } = data;
+module.exports.createCaseStudy = ({caseStudyData}) => {
 
-  const sql = {
-    text: 'INSERT INTO case_studies (fname, lname, title_of_case_study, edition, volume, publisher_name, publication_year) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id',
-    values: [fname, lname, title_of_case_study, edition, volume, publisher_name, publication_year],
-  };
-  console.log('SQL object:', sql.text);
-  console.log('Row Id:', sql.values);
+    const {author_fname, author_lname, title_of_case_study, edition, volume_number, publisher_name, publication_year, 
+        page_number, url_of_case_study, number_of_nmims_authors, nmims_authors, nmims_campus_authors, nmims_school_authors} = caseStudyData;
 
-  return pgPool.query(sql.text, sql.values);
-};
+    let sql = {
+        text : `INSERT INTO case_studies (author_fname, author_lname, title_of_case_study, edition, volume_number, publisher_name, publication_year, 
+            page_number, url_of_case_study, number_of_nmims_authors, nmims_authors, nmims_campus_authors, nmims_school_authors) 
+                VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13) RETURNING id`,
+        values : [author_fname, author_lname, title_of_case_study, edition, volume_number, publisher_name, publication_year, 
+                page_number, url_of_case_study, number_of_nmims_authors, nmims_authors, nmims_campus_authors, nmims_school_authors]
+    };
+    return autoDbW.query(sql);
+}
 
+module.exports.deleteCaseStudy = ({caseStudyId}) => {
+    let sql = {
+        text : 'DELETE FROM case_studies WHERE id = $1',
+        values : [caseStudyId]
+    }
+    return autoDbW.query(sql);
+}
 
+module.exports.UpdateCaseStudyData = async ({caseStudyId , updatedCaseStudyData}) => {
+    const { author_fname, author_lname, title_of_case_study, edition, volume_number, publisher_name, publication_year} = updatedCaseStudyData;
+    let sql = {
+        text : `UPDATE case_studies SET  author_fname = $2 , author_lname = $3 , title_of_case_study = $4 ,
+                edition =  $5, volume_number = $6 , publisher_name = $7 , publication_year = $8 ,
+                page_number = $9 , url_of_case_study = $10 , number_of_nmims_authors = $11 ,
+                nmims_authors = $12 , nmims_campus_authors = $13, nmims_school_authors = $14   WHERE id = $1`,
+        values : [caseStudyId , author_fname, author_lname, title_of_case_study, edition, volume_number, publisher_name, publication_year]
+    }
+    return autoDbW.query(sql);
+}
