591829e1c69d831b65cb27eb2f6156fc29a66b4f
MishraAnkit8 <144013606+MishraAnkit8@users.noreply.github.com>
2023-12-10 23:19:41 +0530

    Update case-study-service.js

--- case-study-service.js
+++ case-study-service.js
@@ -1,47 +1,13 @@
-const caseStudyModel = require('../models/case-study.model');
+const caseStudyModel = require('../models/case-study.models');
+module.exports.createCaseStudy = async () => {
+    const caseStudyFetchData = await caseStudyModel.fetchCaseStudy();
+    console.log(caseStudyFetchData.rows);
+    return caseStudyFetchData.rows;
+}
 
-module.exports.fetchCaseStudy = async() => {
-    const caseStudiesData = await caseStudyModel.renderCaseStudy();
-    return caseStudiesData;
-};
-
-module.exports.insertCaseStudyData = async (body) => {
+module.exports.insertCaseStudies = async (body) => {
     const {caseStudyData} = body;
-    console.log('caseStudies  ::', body);
-    const caseStudiesData = await caseStudyModel.createCaseStudy({caseStudyData});
-    return caseStudiesData ;
-}
-
-module.exports.deleteCaseStudyData = async (caseStudyId) => {
-    const caseStudyData = await caseStudyModel.deleteCaseStudy({caseStudyId});
-    if(caseStudyData.rowCount ==1){
-        return {
-            status : 'done',
-            massage : ' data deleted successfully'
-        }
-    }
-    else{
-        return {
-            status : 'failed',
-            massage : 'failed to delete'
-        }
-    }
-}
-
-module.exports.updateCaseStudies = async ({caseStudyId , updatedCaseStudyData}) => {
-    const updatedCaseStudy = await caseStudyModel.UpdateCaseStudyData({caseStudyId , updatedCaseStudyData});
-    console.log('id For Udation service' , caseStudyId);
-    console.log('updated Data Service ==>', updatedCaseStudyData);
-    if(updatedCaseStudy.rowCount === 1){
-        return {
-            status : 'done' ,
-            massage : 'data updated successfully',
-        }
-    }
-    else{
-        return {
-            status : 'failed' ,
-            massage : 'error data is not updated'
-        }
-    }
+    console.log('inserted data in service ==>>', body);
+    const createCaseStudies = await caseStudyModel.insertDataIntoCaseStudies({caseStudyData});
+    return createCaseStudies ;
 }
