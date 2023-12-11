f5205b21e9edbbae95db2346113f5b9fbb5a443e
MishraAnkit8 <144013606+MishraAnkit8@users.noreply.github.com>
2023-12-11 18:46:34 +0530

    Update caseControoller.js

--- caseControoller.js
+++ caseControoller.js
@@ -1,8 +1,8 @@
-const caseStudyService = require('../services/case-study.service')
+const caseStudyService = require('../services/case-study.service');
 
 module.exports.renderCaseStudy = async(req, res, next) => {
     const caseStudiesDataList = await caseStudyService.createCaseStudy();
-    console.log('data is controller' , caseStudiesDataList);
+    console.log('data in controller' , caseStudiesDataList);
     res.render('case-study', {
         caseStudiesData : caseStudiesDataList
     } );
@@ -23,6 +23,60 @@ module.exports.insertCaseStudies  = async(req, res, next) => {
         res.status(500).send({
             status : 'failed',
             massage : 'Data is Not inserted'
+        })
+    }
+}
+
+module.exports.delCaseStudies = async(req, res, next) => {
+    const caseStudyId = req.body.caseStudyId;
+    console.log('ID ==>', caseStudyId);
+    const caseStudies = await caseStudyService.delCaseStudies(caseStudyId);
+    if(caseStudies.status === 'done'){
+        res.status(200).send({
+            status : 'done',
+            massage : caseStudies.massage 
+        })
+    }
+    else{
+        res.status(500).send({
+            status : 'failed',
+            massage : caseStudies.massage
+        })
+    }
+}
+
+module.exports.caseStudyView = async (req, res, next) => {
+    const casestudyId = req.body.caseStudyId;
+    console.log('CaseID For View' ,casestudyId);
+    const caseStudiesView = await caseStudyService.viewCaseStudies(casestudyId);
+    console.log('data for view', caseStudiesView)
+    if(caseStudiesView){
+        res.status(200).send(caseStudiesView)
+    }
+    else{
+        res.staus(500).send({
+            status : 'Failed',
+            massage : 'failed to view caseStudy'
+
+        })
+    }
+}
+
+module.exports.updatedCaseStudies = async(req, res, next) => {
+    const updatedCaseStudies = req.body;
+    const caseStudyId = req.body.caseStudyId;
+    console.log('caseId For Updation ::', caseStudyId);
+    const updatdeCaseStudiesData = await caseStudyService.updatedCaseStudies({caseStudyId , updatedCaseStudies});
+    if(updatdeCaseStudiesData.status === 'done'){
+        res.status(200).send({
+            status : 'done',
+            massage : updatdeCaseStudiesData.massage
+        })
+    }
+    else{
+        res.status(500).send({
+            status : 'failed',
+            massage : updatdeCaseStudiesData.massage
         })
     }
 }
