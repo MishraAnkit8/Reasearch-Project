a8bf7adbdd6310db89d5efbbd9ed15bd058afd3e
MishraAnkit8 <144013606+MishraAnkit8@users.noreply.github.com>
2023-12-19 18:12:32 +0530

    Update patent-submission.model.js

--- patent-submission.model.js
+++ patent-submission.model.js
@@ -12,12 +12,14 @@ module.exports.fetchPatentSubMissionForms = async() => {
     return autoDbR.query(sql)
 }
 
-module.exports.insertPatentData = async(patentData) => {
+module.exports.insertPatentData = async(patentData,file) => {
     const {type_of_invention, title_of_invention,  patent_stage, achive_sdg, application_no, date, is_presenter} = patentData;
+   
+    console.log('file path', file)
     console.log("patentData::::::", patentData)
     let sql = {
-        text : `INSERT INTO  patent_submissions (type_of_invention, title_of_invention,  patent_stage, achive_sdg, application_no, date,        is_presenter) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id`,
-        values : [type_of_invention, title_of_invention,  patent_stage, achive_sdg, application_no, date, is_presenter]
+        text : `INSERT INTO  patent_submissions (type_of_invention, title_of_invention,  patent_stage, achive_sdg, application_no, date,        is_presenter, myfile) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING id`,
+        values : [type_of_invention, title_of_invention,  patent_stage, achive_sdg, application_no, date, is_presenter, file]
     }
     console.log('sql ==>>', sql)
     return autoDbW.query(sql);
