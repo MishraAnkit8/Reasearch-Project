88c6153ac3eeb90319c1c180a03d03077ede8e0f
MishraAnkit8 <144013606+MishraAnkit8@users.noreply.github.com>
2023-12-20 19:00:12 +0530

    Update patent-submission.model.js

--- patent-submission.model.js
+++ patent-submission.model.js
@@ -18,10 +18,23 @@ module.exports.insertPatentData = async(patentData,file) => {
     console.log('file path', file)
     console.log("patentData::::::", patentData)
     let sql = {
-        text : `INSERT INTO  patent_submissions (type_of_invention, title_of_invention,  patent_stage, achive_sdg, application_no, date,        is_presenter, myfile) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING id`,
+        text : `INSERT INTO  patent_submissions (type_of_invention, title_of_invention,  patent_stage, achive_sdg, application_no, date, is_presenter, patent_file) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING id`,
         values : [type_of_invention, title_of_invention,  patent_stage, achive_sdg, application_no, date, is_presenter, file]
     }
     console.log('sql ==>>', sql)
     return autoDbW.query(sql);
 
+}
+
+module.exports.updatePatentsubmissionData = async({updatedPatentData, patentId, patentDocument}) => {
+    console.log('filename in models ==>', patentDocument )
+    const {type_of_invention, title_of_invention,  patent_stage, achive_sdg, application_no, date, is_presenter} = updatedPatentData 
+    let sql = {
+        text : `UPDATE patent_submissions  SET type_of_invention = $2,  title_of_invention = $3, patent_stage = $4, achive_sdg = $5, 
+              application_no = $6, date = $7, is_presenter =$8 , patent_file = $9 WHERE id = $1`,
+        values : [patentId , type_of_invention, title_of_invention,  patent_stage, achive_sdg, application_no, date, is_presenter, patentDocument]
+    
+    }
+    console.log('Sql ==>>', sql);
+    return autoDbW.query(sql)
 }
