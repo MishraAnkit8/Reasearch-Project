e314572de9c25c73bfc0a8d0a731466e7f38b94f
AnkitKumarMi <ankit.mishra10021997@gmail.com>
2024-10-29 17:12:40 +0530

    commit

--- case-study.route.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const caseStudyController = require('../controller/case-study-con');
-
-router.get('/' ,  caseStudyController.renderData);
-
- router.post('/insert',  caseStudyController.insertTask);
-
-module.exports = router ;
